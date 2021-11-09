b2610fd3e733f587f50394bfd04edc9b050756ef
Alex Michael Maxwell <ALEX.MAXWELL.2019@MUMAIL.IE>
2021-11-09 11:09:20 +0000

    Trying to host app on heroku

--- server.js
+++ server.js
@@ -65,8 +65,8 @@ app.listen(port, () => {
 
 const path = require('path')
 // Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/public')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 // Anything that doesn't match the above, send back index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'))
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
 })
